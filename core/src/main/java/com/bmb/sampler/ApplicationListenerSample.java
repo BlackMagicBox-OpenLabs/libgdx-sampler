e2c1cc733ac3f3a38b261aeb35e865fb9d7ff134
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2024-12-28 03:47:15 +0100

    FEAT Basic project setup

---
+++ core/src/main/java/com/bmb/sampler/ApplicationListenerSample.java
@@ -0,0 +1,36 @@
+package com.bmb.sampler;
+
+
+import com.badlogic.gdx.ApplicationListener;
+
+public class ApplicationListenerSample implements ApplicationListener {
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
